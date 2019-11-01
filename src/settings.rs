1fa63767d18f1a8755b811bf900572b9b7e82134
Harry Barber <harrybarber@protonmail.com>
2019-11-01 21:41:46 +0700

    Add database path to settings

--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,5 @@
 use config::{Config, ConfigError, Environment, File};
+use dirs::home_dir;
 use serde::Deserialize;
 
 use crate::CLI_ARGS;
@@ -9,12 +10,14 @@ pub const DEFAULT_BITCOIN_PASSWORD: &str = "";
 pub const DEFAULT_BIND: &str = "[::1]:50051";
 pub const DEFAULT_BANNER: &str = "Welcome to Photon!";
 pub const DEFAULT_DONATION_ADDRESS: &str = "";
+pub const DEFAULT_DB_PATH: &str = ".photon/db";
 
 #[derive(Debug, Deserialize)]
 pub struct Settings {
     pub bitcoin_rpc: String,
     pub bitcoin_user: String,
     pub bitcoin_password: String,
+    pub db_path: String,
     pub bind: String,
     pub banner: String,
     pub donation_address: String,
@@ -25,13 +28,23 @@ impl Settings {
     pub fn fetch() -> Result<Self, ConfigError> {
         let mut settings = Config::new();
 
+        // Get home directory
+        let home_dir = match dirs::home_dir() {
+            Some(some) => some,
+            None => return Err(ConfigError::Message("no home directory".to_string())),
+        };
+
         // Default settings
-        settings.set("bitcoin_rpc", DEFAULT_BITCOIN_RPC)?;
-        settings.set("bitcoin_user", DEFAULT_BITCOIN_USER)?;
-        settings.set("bitcoin_password", DEFAULT_BITCOIN_PASSWORD)?;
-        settings.set("bind", DEFAULT_BIND)?;
-        settings.set("banner", DEFAULT_BANNER)?;
-        settings.set("donation_address", DEFAULT_DONATION_ADDRESS)?;
+        settings.set_default("bitcoin_rpc", DEFAULT_BITCOIN_RPC)?;
+        settings.set_default("bitcoin_user", DEFAULT_BITCOIN_USER)?;
+        settings.set_default("bitcoin_password", DEFAULT_BITCOIN_PASSWORD)?;
+        settings.set_default("bind", DEFAULT_BIND)?;
+        settings.set_default("banner", DEFAULT_BANNER)?;
+        let mut default_db = home_dir.clone();
+        default_db.push(DEFAULT_DB_PATH);
+        settings.set_default("db_path", default_db.to_str())?;
+        settings.set_default("db_path", DEFAULT_DB_PATH)?;
+        settings.set_default("donation_address", DEFAULT_DONATION_ADDRESS)?;
 
         // Merge with settings file
         settings.merge(File::with_name("config").required(false))?;
@@ -41,22 +54,25 @@ impl Settings {
 
         // Merge with command line arguments
         if let Some(bind) = CLI_ARGS.value_of("bind") {
-            settings.set("bind", bind).unwrap();
+            settings.set("bind", bind)?;
         }
         if let Some(banner) = CLI_ARGS.value_of("banner") {
-            settings.set("banner", banner).unwrap();
-        }
-        if let Some(address) = CLI_ARGS.value_of("donation-address") {
-            settings.set("donation_address", address).unwrap();
+            settings.set("banner", banner)?;
         }
         if let Some(bitcoin_rpc) = CLI_ARGS.value_of("bitcoin-rpc") {
-            settings.set("bitcoin_rpc", bitcoin_rpc).unwrap();
+            settings.set("bitcoin_rpc", bitcoin_rpc)?;
         }
         if let Some(bitcoin_user) = CLI_ARGS.value_of("bitcoin-user") {
-            settings.set("bitcoin_user", bitcoin_user).unwrap();
+            settings.set("bitcoin_user", bitcoin_user)?;
         }
         if let Some(bitcoin_password) = CLI_ARGS.value_of("bitcoin-password") {
-            settings.set("bitcoin_password", bitcoin_password).unwrap();
+            settings.set("bitcoin_password", bitcoin_password)?;
+        }
+        if let Some(db_path) = CLI_ARGS.value_of("db-path") {
+            settings.set("db_path", db_path)?;
+        }
+        if let Some(address) = CLI_ARGS.value_of("donation-address") {
+            settings.set("donation_address", address)?;
         }
         settings.try_into()
     }
